6d400c745c84c6eb36bbdf29baa22e4709fe0e3b
Renata Guerra <renatajarmova@gmail.com>
2021-01-02 21:18:10 +0100

    Create intial 2021 portfolio

---
+++ src/App.svelte
@@ -0,0 +1,72 @@
+<main>
+	<img src="logo.svg" alt="logo" />
+	<h1>Renata Guerra</h1>
+	<p>software developer</p>
+
+	<a
+	href="https://github.com/renatadev"
+	target="_blank"
+	rel="noopener noreferrer"
+  >
+  github
+  </a>
+  <a
+	href="https://twitter.com/renatadev"
+	target="_blank"
+	rel="noopener noreferrer"
+  >
+  twitter
+  </a>
+  <a
+	href="https://linkedin.com/in/renatadev"
+	target="_blank"
+	rel="noopener noreferrer"
+  >
+  linkedin
+  </a>
+  <a
+	href="mailto:contact@renata.dev"
+	target="_blank"
+	rel="noopener noreferrer"
+  >
+  contact
+  </a>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em 0;
+		max-width: 240px;
+		margin: 0 auto; 
+		position: fixed; 
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+		margin: 0;
+	}
+
+	p {
+		text-transform: uppercase;
+		letter-spacing: 1.8px;
+		padding: 6px;
+		margin: 0;
+	}
+
+	a {
+		font-size: 12px;
+		padding-right: 4px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
